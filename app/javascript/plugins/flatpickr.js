ebac4d4f674a43ed4faa64822b675ad664de2861
baptiste lefranc morin <baptistelefrancmorin@gmail.com>
2018-05-30 14:33:52 +0200

    date picker

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,4 +1,8 @@
 import flatpickr from "flatpickr"
-import "flatpickr/dist/flatpickr.min.css" // Note this is important!
+import "flatpickr/dist/themes/material_blue.css" // Note this is important!
+import rangePlugin from "flatpickr/dist/plugins/rangePlugin"
 
-flatpickr(".datepicker", {})
+flatpickr("#start_date", {
+  altInput: true,
+  plugins: [new rangePlugin({ input: "#end_date"})]
+})
