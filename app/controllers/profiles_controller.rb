c99377d7b9b99155de65527c7a046df6b9d2fb21
Basile Marquefave <basile.marquefave@gmail.com>
2018-05-31 20:02:42 +0200

    user_profile

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,17 @@
+class ProfilesController < ApplicationController
+  def edit
+    authorize(current_user)
+  end
+
+  def update
+    authorize current_user
+    current_user.update(user_params)
+    redirect_to housings_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:street, :zip_code, :city, :phone_number, :nationality)
+  end
+end
--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,4 +1,5 @@
 class RentalsController < ApplicationController
+  before_action :user_info_completed, only: [ :new ]
 
   def new
     @housing = Housing.find(params[:housing_id])
@@ -25,6 +26,13 @@ class RentalsController < ApplicationController
   end
 
   private
+
+  def user_info_completed
+    if current_user.street.nil? && current_user.zip_code.nil? && current_user.city.nil? && current_user.phone_number.nil? && current_user.nationality.nil? && current_user.marital_status.nil?
+      redirect_to profile_edit_path, notice: "Completer vos infos pour pouvoir créer une location"
+      return
+    end
+  end
 
   def rental_params
     params.require(:rental).permit(:furnished, :start_date, :end_date, :monthly_rent, :monthly_expenses)
