a56557f57279497693ac7d51502019a0707ad44a
baptiste lefranc morin <baptistelefrancmorin@gmail.com>
2018-05-31 17:52:54 +0200

    fix small typos

--- app/models/renter.rb
+++ app/models/renter.rb
@@ -4,12 +4,12 @@ class Renter < ApplicationRecord
 
   belongs_to :rental
 
-  validates :gender,         presence: true, inclusion: { in: ["Monsieur", "Madame"] }
+  validates :gender,         presence: true, inclusion: { in: GENDER_TYPE }
   validates :first_name,     presence: true
   validates :last_name,      presence: true
   validates :phone_number,   presence: true, format: { with: /\A0\d{9}/ }
   validates :email,          presence: true, format: { with: /\A.*@.*\.com\z/ }
   validates :nationality,    presence: true
   validates :date_of_birth,  presence: true
-  validates :marital_status, presence: true, inclusion: { in: ["Célibataire", "Marié", "Divorcé"] }
+  validates :marital_status, presence: true, inclusion: { in: MARITAL_STATS_TYPE }
 end
