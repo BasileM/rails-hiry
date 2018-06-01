a6f9b01ec110d2693d0b6229c79257159febb7c8
baptiste lefranc morin <baptistelefrancmorin@gmail.com>
2018-06-01 15:03:43 +0200

    card index housing

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,9 +1,20 @@
 import "bootstrap";
 import "../plugins/flatpickr"
 import { hideFloorFieldIfHousingTypeIsHouse } from '../components/housing';
+import { update_map } from '../packs/map';
 
 flatpickr(".datepicker", {
 
 });
 
 hideFloorFieldIfHousingTypeIsHouse();
+
+
+if (document.getElementById("index_page")) {
+ document.addEventListener("DOMContentLoaded", () => {
+  const mini_maps = document.querySelectorAll(".mini_map");
+    mini_maps.forEach((mini_map) => {
+      update_map(`${mini_map.id}`);
+    });
+  });
+}
