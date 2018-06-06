cbd9e8096b26140981da4d52014244dc7e6b8d93
baptiste lefranc morin <baptistelefrancmorin@gmail.com>
2018-06-06 16:45:49 +0200

    send email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -15,23 +15,13 @@ class UserMailer < ApplicationMailer
     mail to: @renter.email, subject: 'Bonjour'
   end
 
-  def receipt(id) #change the following line of code
+  def inventory(renter)
     @renter = renter
-    temp_pdf_file = Tempfile.new(["contrat-de-location", ".pdf"])
+    temp_pdf_file = Tempfile.new(["etat-des-lieux", ".pdf"])
     File.open(temp_pdf_file, 'wb') do |file|
-      file << open(@renter.rental.lease_pdf.url).read
+      file << open(@renter.rental.inventory_pdf.url).read
     end
-    attachments['contrat-de-location.pdf'] = File.read(temp_pdf_file.path)
-    mail to: @renter.email, subject: 'Bonjour'
-  end
-
-  def inventory(id) #change the following line of code
-    @renter = renter
-    temp_pdf_file = Tempfile.new(["contrat-de-location", ".pdf"])
-    File.open(temp_pdf_file, 'wb') do |file|
-      file << open(@renter.rental.lease_pdf.url).read
-    end
-    attachments['contrat-de-location.pdf'] = File.read(temp_pdf_file.path)
+    attachments['etat-des-lieux.pdf'] = File.read(temp_pdf_file.path)
     mail to: @renter.email, subject: 'Bonjour'
   end
 end
