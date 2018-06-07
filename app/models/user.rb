54c90a851c0d533e122a8838056edbbb04d6a370
baptiste lefranc morin <baptistelefrancmorin@gmail.com>
2018-06-07 11:49:40 +0200

    remove validtion on user date of birth

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,5 +13,5 @@ class User < ApplicationRecord
   has_many :housings
   has_many :rentals, through: :housings
 
-  validates :date_of_birth,  numericality: { only_integer: true, greater_than: 0 }
+  #validates :date_of_birth,  numericality: { only_integer: true, greater_than: 0 }
 end
